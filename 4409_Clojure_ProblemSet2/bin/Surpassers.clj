38c79a3290afedf4c3dcab91f791c28d316c9044
John McCall <John@192.168.1.102>
2013-02-06 19:05:38 -0600

    Finished generateTails, started num-surpassers

--- 4409_Clojure_ProblemSet2/bin/Surpassers.clj
+++ 4409_Clojure_ProblemSet2/bin/Surpassers.clj
@@ -1,2 +1,22 @@
 (ns Surpassers)
+(use 'clojure.test)
 
+(defn generateTails [coll]
+  (loop [toReturn (), current coll]
+    (if (empty? current) 
+      (map #(apply str %) toReturn)
+      (recur (cons current toReturn), (rest current))
+      )
+    )
+  )
+
+
+
+;; Tests for generateTails
+(is (= '("t" "st" "est" "test") (generateTails "test")))
+
+;; Tests for num-surpassers
+(is (= 5 (num-surpassers "generating")))
+(is (= 6 (num-surpassers "enerating")))
+(is (= 2 (num-surpassers "nerating")))
+(is (= 5 (num-surpassers "erating")))
