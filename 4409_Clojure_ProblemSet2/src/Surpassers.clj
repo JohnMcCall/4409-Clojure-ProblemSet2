33c932455b807ac188383147ac83881518144d3b
John McCall <John@192.168.1.102>
2013-02-06 19:19:35 -0600

    Finished num-surpassers, starting max-surpasser-count

--- 4409_Clojure_ProblemSet2/src/Surpassers.clj
+++ 4409_Clojure_ProblemSet2/src/Surpassers.clj
@@ -10,6 +10,9 @@
     )
   )
 
+(defn num-surpassers [coll]
+  (count (filter #(< (int (first coll)) (int %)) (rest coll)))
+  )
 
 
 ;; Tests for generateTails
@@ -20,3 +23,12 @@
 (is (= 6 (num-surpassers "enerating")))
 (is (= 2 (num-surpassers "nerating")))
 (is (= 5 (num-surpassers "erating")))
+
+;; Tests for max-surpasser-count
+(is (= 0 (max-surpasser-count [5])))
+(is (= 0 (max-surpasser-count [\a])))
+(is (= 0 (max-surpasser-count [1 0])))
+(is (= 1 (max-surpasser-count [0 1])))
+; The first \e has six larger characters to its right
+(is (= 6 (max-surpasser-count [\g \e \n \e \r \a \t \i \n \g])))
+(is (= 6 (max-surpasser-count "generating")))
